e714f8d45fc45ea6aad88b4c57d3ed0c781d1191
processid <34098270+processid@users.noreply.github.com>
2021-06-10 09:41:23 +0200

    Premier commit

---
+++ Config.php
@@ -0,0 +1,42 @@
+<?php
+// Valeurs de configuration
+// Par exemple pour ajouter un tableau associatif de valeurs de config:
+// $vars = array('key1'=>'value1','key2'=>'value2');
+// Config::addParams($vars);
+
+namespace processid\config;
+
+class Config {
+    
+    private static $params = array();
+    
+    // Ajoute des paramètres
+    // $array est un tableau associatif
+    static public function addParams($array) {
+        if (is_array($array)) {
+            foreach ($array as $key=>$value) {
+                self::$params[$key] = $value;
+            }
+        }
+    }
+
+    // Supprime un paramètre
+    static public function removeParam($key) {
+        if (isset(self::$params[$key])) {
+            unset(self::$params[$key]);
+        }
+    }
+
+    // Supprime tous les paramètres
+    static public function removeParams() {
+        self::$params = array();
+    }
+    
+    // Retourne la valeur d'un paramètre
+    static public function param($key) {
+        if (isset(self::$params[$key])) {
+            return self::$params[$key];
+        }
+    }
+}
+?>
